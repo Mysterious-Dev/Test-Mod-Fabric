d71e6598f22c46515daec18fdc31e489ab7945d0
Mysterious_Dev <mysteriousdevs@gmail.com>
2020-08-25 19:08:14 +0200

    Add Basic Item

--- src/main/java/fr/lmf/testmodfabric/Main.java
+++ src/main/java/fr/lmf/testmodfabric/Main.java
@@ -1,14 +1,17 @@
 package fr.lmf.testmodfabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
+
+	public static final String MODID = "testmod_fabric";
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier(MODID, "basic_item"), new Item(new Item.Settings().maxCount(32).group(ItemGroup.MISC)));
 	}
 }
