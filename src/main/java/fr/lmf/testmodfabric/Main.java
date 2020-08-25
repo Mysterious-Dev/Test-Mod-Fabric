1fc42ce7739bf5ca07d14513b150122cabf6595d
Mysterious_Dev <mysteriousdevs@gmail.com>
2020-08-25 19:20:57 +0200

    Add Basic Block

--- src/main/java/fr/lmf/testmodfabric/Main.java
+++ src/main/java/fr/lmf/testmodfabric/Main.java
@@ -1,6 +1,11 @@
 package fr.lmf.testmodfabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.block.MaterialColor;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -9,9 +14,18 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 
 	public static final String MODID = "testmod_fabric";
+	private Item BASIC_ITEM;
+	private Block BASIC_BLOCK;
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier(MODID, "basic_item"), new Item(new Item.Settings().maxCount(32).group(ItemGroup.MISC)));
+		BASIC_ITEM = Registry.register(Registry.ITEM, new Identifier(MODID, "basic_item"), new Item(new Item.Settings().maxCount(32).group(ItemGroup.MISC)));
+		BASIC_BLOCK = Registry.register(Registry.BLOCK, new Identifier(MODID, "basic_block"), new Block(AbstractBlock.Settings.of(Material.STONE, MaterialColor.STONE).strength(2.0f, 2.0f)));
+		Registry.register(Registry.ITEM, new Identifier(MODID, "basic_block"), new BlockItem(BASIC_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
+
+
+
 }
+
+
